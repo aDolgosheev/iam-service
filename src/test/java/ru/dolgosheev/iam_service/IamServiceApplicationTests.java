52ab546591a05407ff0d5d0e632aa92c3db95bb6
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2025-04-01 23:04:45 +0300

    2025.04.01 initial commit

---
+++ src/test/java/ru/dolgosheev/iam_service/IamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dolgosheev.iam_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
